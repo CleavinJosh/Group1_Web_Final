45548b1ab544d5734c9fea9b07d11ead6d48e904
Ranielo <sultones5@gmail.com>
2024-06-01 19:49:31 +0800

    add protection on Database crash

--- Model/Database.php
+++ Model/Database.php
@@ -5,13 +5,26 @@ class Database
     protected $conn;
     protected $statement;
 
-    public function __construct( $config, $username = 'root', $password = 'password')
+    public function __construct( $config, $username = 'root', $password = '')
     {
-        $dsn = 'mysql:' . http_build_query( $config, '', ';');
+        try {
 
-        $this->conn = new PDO($dsn, $username, $password, [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]); 
+            $dsn = 'mysql:' . http_build_query( $config, '', ';');
+
+            $this->conn = new PDO($dsn, $username, $password, [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]); 
+
+        }catch(Exception $e) {
+
+            displayError([
+                "error" => 404,
+                "description" => $e,
+                "redirect" => "/index.php/login"
+            ]);
+
+        }
+        
     }
 
 
